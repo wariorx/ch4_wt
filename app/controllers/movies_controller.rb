c49afabd4dc8431792e95d276f0c50676695f45c
wariorx <rcalcamo@uncc.edu>
2017-10-08 21:44:44 +0000

    Done with Section 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,19 @@
+#This is movies_controller.rb
+class MoviesController < ApplicationController
+
+	def index
+		@movies = Movie.all
+	end
+
+	def show
+		id = parms[:id] #retrieve movie ID from URI route
+		@movie = Movie.find(id) #look up movie by unique ID
+		#renders app/views/movies.show.html.haml by default
+	end
+	
+	private
+	
+		def movie_params
+    		params.require(:movie).permit(:title, :rating, :description, :release_date)
+		end
+end
