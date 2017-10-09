fe89c6a1f908abb0ccdf5661516c006b3e26a246
wariorx <rcalcamo@uncc.edu>
2017-10-09 02:20:23 +0000

    Done with Section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,13 +16,33 @@ class MoviesController < ApplicationController
 		#default: render new template
 	end
 	
-def create
-    #@movie = Movie.create!(params[:movie]) #old way
-    @movie = Movie.create!(movie_params)  # new way
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
+	def create
+    	#@movie = Movie.create!(params[:movie]) #old way
+    	@movie = Movie.create!(movie_params)  # new way
+    	flash[:notice] = "#{@movie.title} was successfully created."
+    	redirect_to movies_path
+	end
+	
+	def edit
+		@movie = Movie.find params[:id]
+	end
+	
+	def update
+    	@movie = Movie.find params[:id]
+	    #@movie.update_attributes!(params[:movie])  # old way
+	    @movie.update_attributes!(movie_params)  # new way  
+	    flash[:notice] = "#{@movie.title} was successfully updated."
+	    redirect_to movie_path(@movie)
+	end
 
+	
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:notice] = "Movie #{@movie.title} deleted"
+		
+		redirect_to movies_path
+	end
 	
 	private
 	
